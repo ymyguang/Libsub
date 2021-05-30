b09ac1dfe5da9c1899dac01f938f43cb343b3bc8
有光 <2096304869@qq.com>
2021-05-30 09:10:02 +0800

    add function

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-import random
 import time
 from Cancel import BespeakCancel_nomal
 from Find import search
@@ -25,14 +24,14 @@ def findSeat():
 
 # 刷新预约时间
 def refresh(seatNum):
-    print(printLog.get_time(), BespeakCancel_nomal.BespeakCancel())
+    print(printLog.get_time(), "取消结果：", BespeakCancel_nomal.BespeakCancel())
     sub.subscribe(seatNum)
+    # 循环等待，防止10分钟内取消无效；
     if getInfo.getSeatText():
         start = time.time()
-        # 循环等待，防止10分钟内取消无效；
         for i in range(0, 15):
             now = time.time()  # 当前时间
-            print(printLog.get_time(), "到馆时间将于{}分钟后刷新".format(10 - i))
+            print(printLog.get_time(), "到馆时间将于{}分钟后刷新".format(11 - i))
             if now - start >= 60 * 10.1:  # 若开始时间距离现在时间大于10分钟后，取消循环等待，直接退出，进行下一次预约操作
                 break
             else:
@@ -60,20 +59,33 @@ def Cancel():
     print(printLog.get_time(), "\033[1;40;42m{}\033[0m".format(cancel))
 
 
-# 检测位置状态
-def main():
+# 寻找走廊
+def corridor():
     while 1:
         seatNum = getInfo.getSeatText()
-        if seatNum and isOk(seatNum[:-3]):
+        if seatNum and isOk(seatNum[:-3]):  # 有位置，且在走廊，保持当前状态
             refresh(seatNum)
             continue
-        elif seatNum:
+        elif seatNum:  # 有位置，但是未在走廊，取消当前位置，重新预约
             Cancel()  # 保证取消成功
-        refresh(findSeat())
+        refresh(findSeat())  # 未找到位置
+
+
+def maintain():
+    while 1:
+        seatNum = getInfo.getSeatText()
+        refresh(seatNum)
 
 
 if __name__ == '__main__':
-    main()
+    print("You current seat information：", getInfo.getSeatNum())
+    option_row = input("Please select state  \n1.corridor \n2.maintain(You must have seat yet)\n")
+    option = int(option_row)
+    if option == 1:
+        corridor()
+    elif option == 2:
+        maintain()
+
     # getInfo.getSeatText()
 # findSeat()
 # Deprecated Code
