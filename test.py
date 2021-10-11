b0859087ea469e9e3ddb85ed90c9127cfad6e292
有光 <2096304869@qq.com>
2021-10-11 21:45:23 +0800

    优化消息提醒机制

--- test.py
+++ test.py
@@ -1,15 +1,4 @@
-import time
+from tools import feedback
 
-from tools import fileExam
-
-i = 1
-def dsx():
-    while 1:
-        global i
-        print("当前i值:", i)
-        i = input()
-        time.sleep(1)
-
-
-if __name__ == '__main__':
-    dsx()
+# for i in range(10,1):
+feedback.feedback(111, "G")
--- tools/feedback.py
+++ tools/feedback.py
@@ -1,28 +1,63 @@
 from tools import printLog
 import requests
 
+qq = {
+    'M': 2096304869,
+    "G": 708227196
+}
 
-def feedback(text, case='M', wx=0):
+way = {
+    "M": "send",
+    "G": "group"
+}
+
+
+def feedback(text, case='M'):
+    text = str(text)
     print(printLog.get_time("Feedback"), "->【", text + ' 】')
-    #return
-    URL = "https://sctapi.ftqq.com/SCT33679Td3sATvBjES3VjKQeZgcsbxeB.send"
+    # return
+    params1 = {
+        "msg": text,
+        "qq": qq[case],
+    }
 
-    if case == 'M':
-        params1 = {
-            "msg": text,
-            "qq": 2096304869,
-        }
-        requests.get("https://qmsg.zendee.cn/send/d105a92ecd34dab1427db4dc4936e339", params=params1)
+    # QQ推送
+    url = "https://qmsg.zendee.cn/" + way[case] + "/d105a92ecd34dab1427db4dc4936e339"
+    c = requests.get(url=url, params=params1)
+    print(printLog.get_time("feedback"), "推送目标QQ:{}".format(str(qq[case])))
+    status = c.json()['success']
+    print(printLog.get_time("feedback"), "QQ推送状态：{},详情：{}".format(c.json()['success'], c.json()['reason']))
 
-    elif case == 'G':
-        params1 = {
-            "msg": text,
-            "qq": 708227196,
-        }
-    requests.get("https://qmsg.zendee.cn/group/d105a92ecd34dab1427db4dc4936e339", params=params1)
+    # QQ推送失败
+    if status is False:
 
-    if wx == 1:
-        params = {
-            "title": text,
-        }
-        requests.get(url=URL, params=params)
+        # coolPush推送
+        print(printLog.get_time("feedback"), "QQ推送失败，进入coolPush推送")
+        t = requests.post("https://push.xuthus.cc/ww/ce4e2dfe9a211ca36f718441f089a88c", data=text.encode("utf-8"))
+        status = t.json()['message']
+        flag = str(status).find("等待执行")
+
+        print(printLog.get_time("feedback"), "coolPush推送状态:", status[0:flag - 1])
+
+        # 未找到：成功关键字 -》推送失败
+        if flag == -1:
+
+            # server酱推送
+            print(printLog.get_time("feedback"), "推送状态：失败 -> 当前已进入Server酱推送")
+            params = {
+                "title": text,
+            }
+            s = requests.get(url="https://sctapi.ftqq.com/SCT33679Td3sATvBjES3VjKQeZgcsbxeB.send", params=params)
+            print(printLog.get_time('feedback'), s.json()['message'])
+
+            # 含有超出限额关键词  -》推送失败
+            if str(s.json()['message']).find("超过当天的发") != -1:
+                # 最后推送
+                print(printLog.get_time("feedback"), "执行最后coolPush推送")
+                t = requests.post("https://push.xuthus.cc/ww/ce4e2dfe9a211ca36f718441f089a88c",
+                                  data=text.encode("utf-8"))
+                status = t.json()['message']
+                print(printLog.get_time("feedback"), "coolPush推送状态:", status)
+
+        else:
+            print(printLog.get_time("feedback"), "推送状态：成功")
