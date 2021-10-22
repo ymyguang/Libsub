9bce143aed980ce3f669d0ba9f1e84a2c959b9e7
有光 <2096304869@qq.com>
2021-10-22 16:44:34 +0800

    调搜索位置参数

--- Find/search.py
+++ Find/search.py
@@ -43,7 +43,7 @@ def search(place):
         seatNumArray = extra(tar, place)  # 当前楼层的可用位置,0全部位置，1位走廊位置
         if seatNumArray != -1:
             seatNumArray.sort()
-            index = int(len(seatNumArray) * 0.8)
+            index = int(len(seatNumArray) * 0.64)
             seatNum = seatNumArray[index]  # 靠后位置
             if roomNumber == "101005" and seatNum[-3:] in ('065', '066', '067'):
                 print(printLog.get_time('find'), "扫描位置为{}-{}，该位置无电源，已跳过！".format(roomName, seatNum[-3:]))
